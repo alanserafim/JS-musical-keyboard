596e5efb8c1cd296b5beeee3a64f05f2de300920
Alan Serafim <97226793+alanserafim@users.noreply.github.com>
2022-04-13 10:33:27 -0300

    Adicionando sons as teclas

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 function tocaSom(){
     document.querySelector('.tecla_pom');  
-    document.querySelector('som_tecla_pom').play();
+    document.querySelector('#som_tecla_pom').play();
 };
+
+function tocaSomClap(){
+    document.querySelector('.tecla_clap');
+    document.querySelector('#som_tecla_clap').play();
+}
+
